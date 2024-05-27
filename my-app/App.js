79a89efc54c07f17b93d94c1ab818ecfb395065f
DylanGates <Solomonafoh@gmail.com>
2024-05-27 16:07:54 +0000

    Changed Font Size of Text

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Mawutor</Text>
+      <Text style={{fontSize: 24}}>My name is Mawutor</Text>
       <StatusBar style="auto" />
     </View>
   );
